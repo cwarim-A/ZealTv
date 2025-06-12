3c4820e558cbc1aca53f95757d65d2d63abd5959
cwarim-A <akoladequam3@gmail.com>
2025-06-12 09:55:01 +0100

    n

--- src/pages/Subscription.jsx
+++ src/pages/Subscription.jsx
@@ -3,8 +3,8 @@ import { CiUser } from 'react-icons/ci'
 
 const Subscription = () => {
   return (
-    <div className='m-5'>
-      <div style={{}}>
+    <div className='p-10'>
+      <div className='px-6 py-10' style={{ border: "1px solid #34333B" }}>
 
       </div>
     </div>
